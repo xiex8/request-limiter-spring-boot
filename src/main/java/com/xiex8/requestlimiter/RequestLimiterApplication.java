55a375956617da550ffb2d9258360ad1d79a815c
xiex8 <sirixie1998@gmail.com>
2023-04-11 22:48:38 -0400

    Initial commit

---
+++ src/main/java/com/xiex8/requestlimiter/RequestLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.xiex8.requestlimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestLimiterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiex8/requestlimiter/RequestLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiex8.requestlimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
